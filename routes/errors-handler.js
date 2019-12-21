f94bdb8f27f31e5e8f48d4058b8a8f55a98dbc5a
Omri Kornblau <omri.354@gmail.com>
2019-12-21 10:49:33 +0200

    Make error handling more configurable

--- routes/errors-handler.js
+++ routes/errors-handler.js
@@ -1,11 +1,23 @@
-const routeError = (req, res, next) => err => {
-  if (err.isBoom) {
-    res.status(err.output.statusCode).send(err.output.payload);
-  } else if (err.isJoi) {
-    res.status(400).send(err);
-  } else {
-    next(err);
+const errorRoutes = [
+  {
+    condition: err => err.isBoom,
+    handler: (res, err) => res.status(err.output.statusCode)
+      .send(err.output.payload)
+  },
+  {
+    condition: err => err.isJoi,
+    handler: (res, err) => res.status(400).send(err)
   }
+];
+
+const routeError = (req, res, next) => err => {
+  const matchedAnyError = errorRoutes.some(route => {
+    if (route.condition(err)) {
+      route.handler(res, err);
+      return true;
+    }
+  });
+  return matchedAnyError ? null : next(err.message)
 }
 
 const handleAsyncErrors = fn => (req, res, next) => {
