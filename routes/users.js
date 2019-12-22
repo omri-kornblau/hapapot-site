0bbe237db8dd240ded675016eae13c62eebe6e33
Omri Kornblau <omri.354@gmail.com>
2019-12-22 18:24:53 +0200

    Add update functionality to users

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,35 @@
 const Mongoose = require('mongoose');
 const Express = require('express');
 
+const withAuth = require('../middleware');
+
 const router = Express.Router()
 const UserModel = Mongoose.model('User');
 
+const sendUser = (res, status, userFromDb) => {
+  res.status(status).send({
+    error: false,
+    user: userFromDb
+  });
+}
+
 router.get(`/user`, async (req, res) => {
-  const users = await UserModel.find();
-  return res.send(users);
+  const { username } = req;
+  const user = await UserModel.findOne({ username })
+  return res.send(user);
 });
 
 router.post(`/user`, async (req, res) => {
-  const user = await UserModel.create(req.body);
-  return res.status(201).send({
-    error: false,
-    user
-  });
+  const user = req.body;
+  const existingUsers = await UserModel.find({ username: user.username });
+
+  if (existingUsers.length > 0) {
+    const updatedUser =
+      await UserModel.updateOne({ username: user.username}, user);
+    return sendUser(res, 204, updatedUser);
+  }
+  const createdUser = await UserModel.create(req.body);
+  return sendUser(res, 201, createdUser);
 });
 
 module.exports = router
