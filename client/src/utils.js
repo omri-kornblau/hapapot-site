6e3393a161051b5be4160f9a3b603524fd04fc90
ido <ido.gedanken@gmail.com>
2019-12-24 19:03:11 +0200

    Added  format time to utils

--- client/src/utils.js
+++ client/src/utils.js
@@ -7,4 +7,9 @@ Utils.formatDate = isoDate => {
   return dateTime.format("DD/MM/YYYY");
 };
 
+Utils.formatTime = isoDate => {
+  const dateTime = moment(isoDate);
+  return dateTime.format("HH:mm");
+};
+
 module.exports = Utils;
