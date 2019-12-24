f85b99ce0cb02d681a7927f67ccac5a4b9c85ba0
ido <ido.gedanken@gmail.com>
2019-12-24 19:02:25 +0200

    Added day and event defaults

--- client/src/defaults/defaults.js
+++ client/src/defaults/defaults.js
@@ -13,4 +13,21 @@ Defaults.UserData = {
   aboutme: ""
 };
 
+Defaults.Day = {
+  date: new Date(),
+  events: [],
+  users: [],
+  rating: 0
+};
+
+Defaults.event = {
+  name: "מסיבה",
+  eventkey: "shalom",
+  icon: "icon-bag-16",
+  cars: { ido: ["omrik"] },
+  items: {},
+  time: new Date(),
+  moneylink: ""
+};
+
 module.exports = Defaults;
