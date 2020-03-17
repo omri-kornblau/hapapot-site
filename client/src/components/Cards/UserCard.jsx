c7bca48d1a6557dcbb80751729365a23592a78fd
User <ido.gedanken@gmail.com>
2020-03-17 09:36:07 -0700

    Move about-us request to helper & Add expand user card func

--- client/src/components/Cards/UserCard.jsx
+++ client/src/components/Cards/UserCard.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { Card, CardFooter, CardBody, CardText } from "reactstrap";
+import { 
+  Card,
+  CardFooter,
+  CardBody, 
+  CardText, 
+  Col
+   } from "reactstrap";
 
 import Utils from "../../utils";
 
@@ -7,7 +13,8 @@ class UserCard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      userData: props.userdata
+      userData: props.userdata,
+      isOpen : false
     };
   }
   componentWillReceiveProps(props) {
@@ -18,30 +25,61 @@ class UserCard extends React.Component {
   getCardBackgroundColor() {
     return this.state.userData.single ? "bg-success" : "bg-danger";
   }
+  renderOpenCard(){
+    return(<div >      
+      <h5 className="title">{Utils.pickNickName(this.state.userData.nicknames)}</h5>
+        <p className="birthday">
+          {Utils.formatDate(this.state.userData.birthday)}
+        </p>
+        <p>
+          {this.state.userData.aboutme}
+        </p>
+      </div>
+
+    );
+  }
+  renderClosedCard(){
+    return(<div>      
+            <h5 className="title">{Utils.pickNickName(this.state.userData.nicknames)}</h5>
+              <p className="birthday">
+                {Utils.formatDate(this.state.userData.birthday)}
+              </p>
+            </div>
+    );
+  }
+  onBlur= () => {
+    this.setState({isOpen : false})
+  }
+
+  onFocus = e => {
+    this.setState({isOpen : true})
+  }
 
   render() {
     return (
+      <Col key={this.state.userData.username}
+        onFocus = {this.onFocus}  
+        onBlur = {this.onBlur}
+        tabIndex = {1}
+        xs = {this.state.isOpen? 12 : 6} >
       <Card {...this.props}>
         <CardBody>
           <CardText />
-          <div className="author">
-            <div className="block block-one" />
-            <div className="block block-two" />
-            <div className="block block-three" />
-            <div className="block block-four" />
-            <a href="#pablo" onClick={e => e.preventDefault()}>
-              <img
-                alt="..."
-                className="avatar"
-                src={require("assets/img/emilyz.jpg")}
-              />
-              <h5 className="title">{Utils.pickNickName(this.state.userData.nicknames)}</h5>
-            </a>
-            <p className="birthday">
-              {Utils.formatDate(this.state.userData.birthday)}
-            </p>
-          </div>
-          <div className="card-description"></div>
+            <div className="author">
+              <div className="block block-one" />
+              <div className="block block-two" />
+              <div className="block block-three" />
+              <div className="block block-four" />
+              <a href="#pablo" onClick={e => e.preventDefault()}>
+                <img
+                  alt="..."
+                  className="avatar"
+                  src={require("assets/img/emilyz.jpg")}
+                  />
+              </a>
+            </div>
+              {this.state.isOpen? this.renderOpenCard() : this.renderClosedCard()}
+            <div className="card-description"></div>
         </CardBody>
         <CardFooter className={this.getCardBackgroundColor()}>
           {/*<div className="button-container">
@@ -54,6 +92,7 @@ class UserCard extends React.Component {
           </div>*/}
         </CardFooter>
       </Card>
+      </Col>
     );
   }
 }
