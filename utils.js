2b80df1fc7d4339d1e420f0a827d075e76066527
Omri Kornblau <omri.354@gmail.com>
2020-04-11 22:45:22 +0300

    Change cookies method

--- utils.js
+++ utils.js
@@ -5,6 +5,10 @@ exports.dateToDayQuery = date => {
   return moment(date).format("YYYY-MM-DD");
 }
 
+exports.getRandomPassword = length => {
+  return Math.random().toString(36).slice(-1 * length);
+}
+
 exports.getBroughtAmount = item => {
   return _.reduce(item.users, (sum, user) => sum + user.amount, 0);
 }
